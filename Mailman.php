0ccd0949de330e6ca6e5c9f55a87afb8a61184f0
Toni Hermoso Pulido <toniher@cau.cat>
2014-10-25 21:24:36 +0200

    add Mailman

---
+++ Mailman.php
@@ -0,0 +1,41 @@
+<?php
+
+
+if (!defined('MEDIAWIKI')) {
+        die('Not an entry point.');
+}
+
+$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
+                'path' => __FILE__,
+                'name' => "Mailman",
+                'description' => "mailman-desc",
+                'version' => 0.1, 
+                'author' => array("Toniher", "Mohr"),
+                'url' => "https://mediawiki.org/wiki/Extension:Mailman",
+);
+
+$GLOBALS['wgExtensionFunctions'][] = "wfMailmanExtension";
+$GLOBALS['wgMessagesDirs']['Mailman'] = __DIR__ . '/i18n';
+$GLOBALS['wgExtensionMessagesFiles']['Mailman'] = dirname( __FILE__ ) . '/Mailman.i18n.php';
+
+
+function wfMailmanExtension() {
+	global $wgParser;
+	$wgParser->setHook( "mailman", "printMailmanForm" );
+}
+
+function printMailmanForm( $input, $argv, $parser, $frame ) {
+
+	// Substitute listinfo -> subscribe
+	$input = $parser->recursiveTagParse( $input, $frame );
+	$input = preg_replace('/listinfo/', 'subscribe', $input);
+	$input = strip_tags( $input );
+	
+	
+	$output = "<form action=\"$input\" method=\"post\">".
+	  "<input name=\"email\" type=\"text\" value=\"".wfMessage("mailman-email")->escaped()."\" onfocus=\"cleartext(this)\" class='mailman-extension' />".
+	  "<input name=\"email-button\" type=\"submit\" value=\"".wfMessage("mailman-subscribe")->escaped()."\" />".
+	  "</form>";
+	return $output;
+}
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ mediawiki-Mailman
 =================
 
 MediaWiki extension for printing Mailman subscription field
+
+Originally based in code by user Mohrr
