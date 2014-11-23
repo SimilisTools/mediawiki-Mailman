e0dea5d964b3cc813bd009bc9f954d5a19d9b0f9
Toni Hermoso Pulido <toniher@cau.cat>
2014-11-23 17:01:32 +0100

    update

--- Mailman.php
+++ Mailman.php
@@ -5,19 +5,23 @@ if (!defined('MEDIAWIKI')) {
         die('Not an entry point.');
 }
 
-$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
-                'path' => __FILE__,
-                'name' => "Mailman",
-                'description' => "mailman-desc",
-                'version' => 0.1, 
-                'author' => array("Toniher", "Mohr"),
-                'url' => "https://mediawiki.org/wiki/Extension:Mailman",
-);
+//self executing anonymous function to prevent global scope assumptions
+call_user_func( function() {
 
-$GLOBALS['wgExtensionFunctions'][] = "wfMailmanExtension";
-$GLOBALS['wgMessagesDirs']['Mailman'] = __DIR__ . '/i18n';
-$GLOBALS['wgExtensionMessagesFiles']['Mailman'] = dirname( __FILE__ ) . '/Mailman.i18n.php';
+	$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
+		            'path' => __FILE__,
+		            'name' => "Mailman",
+		            'description' => "mailman-desc",
+		            'version' => 0.1, 
+		            'author' => array("Toniher", "Mohr"),
+		            'url' => "https://mediawiki.org/wiki/Extension:Mailman",
+	);
 
+	$GLOBALS['wgExtensionFunctions'][] = "wfMailmanExtension";
+	$GLOBALS['wgMessagesDirs']['Mailman'] = __DIR__ . '/i18n';
+	$GLOBALS['wgExtensionMessagesFiles']['Mailman'] = dirname( __FILE__ ) . '/Mailman.i18n.php';
+
+} );
 
 function wfMailmanExtension() {
 	global $wgParser;
@@ -27,6 +31,7 @@ function wfMailmanExtension() {
 function printMailmanForm( $input, $argv, $parser, $frame ) {
 
 	// Substitute listinfo -> subscribe
+	// More improvement possible here
 	$input = $parser->recursiveTagParse( $input, $frame );
 	$input = preg_replace('/listinfo/', 'subscribe', $input);
 	$input = strip_tags( $input );
