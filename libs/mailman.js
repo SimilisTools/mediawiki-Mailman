39b1445147ead5dc8d9554eb8abf7a64fa153b33
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-01 00:28:50 +0100

    message

--- libs/mailman.js
+++ libs/mailman.js
@@ -1,28 +1,39 @@
 $( document ).ready(function() {
 
 
-	$("input.mailman-extension").bind("enterKey",function(e){
+	$(".mailman-mw input.mailman-extension").bind("enterKey",function(e){
 		var email = $(this).val();
 		var action = $(this).data('action');
 		
 		if ( action && email ) {
-			mailmanPost( action, email );
+			mailmanPost( action, email, this );
 		}
 	});
-	$("input.mailman-extension").keyup(function(e){
+	$(".mailman-mw input.mailman-extension").keyup(function(e){
 		if(e.keyCode == 13)
 		{
 			$(this).trigger("enterKey");
 		}
 	});
+	
+	$(".mailman-mw").on( "focus", "input.mailman-extension", function(){
+		$(this).attr("value", "");
+	});
 
-	function mailmanPost( action, email ) {
-		$.post( action, { email: email })
-		.done(function( data ) {
-			console.log( data ); // TODO: Here returned HTML
-		});
+	$( ".mailman-mw" ).on( "click", "input[type='button']", function() {
+
+		var input = $( $(this).parent().find("input.mailman-extension") ).get(0);
+		var email = $(input).val();
+		var action = $(input).data('action');
+		
+		if ( action && email ) {
+			mailmanPost( action, email, input );
+		}
+	});
+
+	function mailmanPost( action, email, context ) {
+		$.post( action, { email: email });
+		$(context).parent().append("<p class='mailman-message'>Subscribed. Check your email</p>");
 	}
 
-	// TODO: Pending button click
-	
 });
